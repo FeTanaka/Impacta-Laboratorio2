e07fd4b457008733581a4c5c2dfb10cdc7e0a375
curso <curso@curso.com>
2022-07-11 20:58:57 -0300

    resolução

---
+++ app/src/main/java/br/com/impacta/laboratorio2/Main.kt
@@ -0,0 +1,9 @@
+package br.com.impacta.laboratorio2
+
+fun main() {
+    var nome = "Impacta"
+    var mensagem = "Seja bem vindo"
+
+    println(mensagem + ", " + nome)
+    println("$mensagem, $nome")
+}
